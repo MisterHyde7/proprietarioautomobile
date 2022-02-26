8c87d39f8b88803051a15805a71cc0e1b7ea6994
MisterHyde7 <luca.martucci@hotmail.com>
2022-02-26 09:37:43 +0100

    carico test list all di proprietario

--- src/it/proprietarioautomobile/test/Test.java
+++ src/it/proprietarioautomobile/test/Test.java
@@ -1,5 +1,8 @@
 package it.proprietarioautomobile.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import it.proprietarioautomobile.dao.EntityManagerUtil;
 import it.proprietarioautomobile.model.Proprietario;
 import it.proprietarioautomobile.service.MyServiceFactory;
@@ -16,6 +19,8 @@ public class Test {
 		try {
 
 			testInserisciProprietario(proprietarioService);
+
+			testListAllProprietario(proprietarioService);
 
 		} catch (Throwable e) {
 			e.printStackTrace();
@@ -36,6 +41,19 @@ public class Test {
 		proprietarioService.inserisciNuovo(proprietarioDaInserire);
 		if (proprietarioDaInserire.getId() == null)
 			throw new RuntimeException("test di inserimento fallito, record non inserito");
+		System.out.println("----- test eseguito con successo -----");
+	}
+
+	private static void testListAllProprietario(ProprietarioService proprietarioService) throws Exception {
+		System.out.println("----- Inizio test list proprietario -----");
+		Proprietario proprietarioDaInserire = new Proprietario("test list", "test list", "test list",
+				new java.util.Date(2022, 01, 01));
+		if (proprietarioDaInserire.getId() != null)
+			throw new RuntimeException("test di inserimento fallito, record gia presente");
+		List<Proprietario> listaDiProprietari = new ArrayList<Proprietario>();
+		listaDiProprietari = proprietarioService.listAllProprietari();
+		if (listaDiProprietari == null || listaDiProprietari.isEmpty())
+			throw new RuntimeException("lista di proprietari vuota");
 		System.out.println("----- test eseguito con successo -----");
 	}
 
